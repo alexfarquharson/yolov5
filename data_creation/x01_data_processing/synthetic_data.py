44786ae0497fb9c6d1a3520e6ba1946de5c63aad
Alexander Faqruharson <farquharsonar@gmail.com>
2023-08-05 11:56:56 +0100

    experiments: data creation setup 05082023

--- data_creation/x01_data_processing/synthetic_data.py
+++ data_creation/x01_data_processing/synthetic_data.py
@@ -56,7 +56,8 @@ class RectangleObject:
         self.width = width
 
 
-def create_particles(frames, image_w, image_h, image_d, offset, label_list, diameter_mean, diameter_std, density_range, n_list = None, distance = None, luminosity_range = 1, fixed_camera_distance = 10):
+def create_particles(frames, image_w, image_h, image_d, offset, label_list, diameter_mean, diameter_std, density_range, n_list = None, 
+                     distance_factor = 1, luminosity_range = 1, fixed_camera_distance = 10, min_dist_list = []):
     ''' 
     input:
         frames: no. pictures
@@ -96,9 +97,8 @@ def create_particles(frames, image_w, image_h, image_d, offset, label_list, diam
     if not (len(luminosity_range) == 1) | (len(luminosity_range) == 2):
         raise ValueError('luminosity_range must either be list of length 1 or 2')
     
-    # set min dist as 4 std from 2 particle centroids
-    if distance == None:
-        distance = 2*diameter_mean
+    # set min dist as 4 std from 2 particle centroids * distance factor (=1 then 4 std)
+    distance = 2*diameter_mean * distance_factor
     diameter_range = [diameter_mean, diameter_std]
     
 
@@ -107,12 +107,11 @@ def create_particles(frames, image_w, image_h, image_d, offset, label_list, diam
         
         # ensure picture within density boundaries
         density = 0
-        while density < density_range[0] or density > density_range[1]:
+        while (density < density_range[0]) | (density > density_range[1]):
     #         get random x,y positions of next particle
             x_y_positions = np.random.random(2)*[image_w - 2*offset, image_h - 2*offset] + offset
     #         get random z position of all particles
             z_positions = np.random.randint(0, image_d, n_list[0])
-            
     #             get fixed diameter of particle set by user (relative diameter size can be between [0] and [1])
             if len(diameter_range) == 2:
                 fixed_diameters = np.random.normal(diameter_range[0], diameter_range[1], n_list[0])
@@ -147,6 +146,9 @@ def create_particles(frames, image_w, image_h, image_d, offset, label_list, diam
                 pos = x_y_positions.reshape(int(len(x_y_positions)/2), 2)
                 d = pos - new_pos
                 min_distance = np.sqrt(np.sum(d*d, axis=1)).min()
+                if min_distance > distance:
+                    min_dist_list.append(min_distance)
+
             x_y_positions = np.append(x_y_positions, new_pos)
 
 #         create object instances given the x,y,z position, label, diameter and intensity
@@ -154,7 +156,103 @@ def create_particles(frames, image_w, image_h, image_d, offset, label_list, diam
         label_list = np.repeat(label_list, n_list).tolist()
         objects.append([SpotObject(x,y,z, label, diameter, theta) for (x, y), z, label, diameter, theta in zip(x_y_positions, z_positions, label_list, diameters, intensities)])
      
-    return np.array(objects)
+    return np.array(objects), min_dist_list
+
+
+
+def create_particles_custom_overlap(frames, image_w, image_h, image_d, offset, label_list, diameter_mean, diameter_std, density_range, n_list = None, 
+                     distance_factor = 1, max_distance_factor = 10000, luminosity_range = 1, fixed_camera_distance = 10, min_dist_list = []):
+    ''' 
+    input:
+        frames: no. pictures
+        n_list: [number of particles type 1, number of particles type 2]
+        image_w: image width
+        image_h: image height
+        image_d: image depth (particles will be placed at different d -which will affect luminosty and size respectively).
+            If set to 1, then there will be no variance in d (and luminosity) and no change to d or luminosity.
+        distance_factor: min no. of particle stds x_y euclidean between any two particles
+        max_distance_factor: max no. of particle stds x_y euclidean between any two particles
+        offset: random distance (must be < image_w/2)
+        label_list: list of particle types (must be same length as n_list)
+        diameter_mean: int (normal dist particle diameter mean)
+        diamaeter_std: int (normal dist particle diameter std)
+        density_range: list [min, max] area density of particles
+        luminosity_range: int or range list of relative particle luminosity (e.g. [1] (no change), [0.9,1] will vary from 90 to 100%)
+        fixed_camera_distance: ditance of image to camera
+        
+    output:
+        array of Objects
+
+    '''
+    # Check and process inputs
+    density_range.sort()
+    # come up with density here and use it to set no. particles
+    if n_list == None:
+        n_list = []
+        proxy_density = np.random.uniform(density_range[0], density_range[1])
+        n_list = [int(np.floor((proxy_density * (image_w * image_h)) / (pi * diameter_mean ** 2)))]
+    if not isinstance(n_list, list): 
+        n_list = [n_list]
+    if not isinstance(label_list, list):
+        label_list = [label_list]
+    if len(n_list) != len(label_list):
+        raise ValueError('The lists must have equal length')
+    if not isinstance(luminosity_range, list):
+        luminosity_range = [luminosity_range, 1]
+    if not (len(luminosity_range) == 1) | (len(luminosity_range) == 2):
+        raise ValueError('luminosity_range must either be list of length 1 or 2')
+    
+    # set min dist as 4 std from 2 particle centroids * distance factor (=1 then 4 std)
+    min_distance_param = 2*diameter_mean * distance_factor
+    max_distance_param = 2*diameter_mean * max_distance_factor
+    diameter_range = [diameter_mean, diameter_std]
+    
+    n_list = [2]
+
+    objects = []
+    for _ in range(frames):
+    
+#         get random x,y positions of next particle
+        x_y_positions = np.random.random(2)*[image_w - 2*offset, image_h - 2*offset] + offset
+#         get random z position of all particles
+        z_positions = np.random.randint(0, image_d, n_list[0])
+        
+#             get fixed diameter of particle set by user (relative diameter size can be between [0] and [1])
+        if len(diameter_range) == 2:
+            fixed_diameters = np.random.normal(diameter_range[0], diameter_range[1], n_list[0])
+        else:
+            fixed_diameters = np.ones(n_list[0]) * diameter_range[0]
+
+#             get diameter and intensity given the z coordinate
+        camera_distance = np.ones(n_list[0]) * fixed_camera_distance
+        fixed_intensity = np.random.uniform(luminosity_range[0], luminosity_range[1], n_list[0])
+        diameters = (fixed_diameters * camera_distance) / (camera_distance + z_positions)
+        # ensure that diameter is not outside of 1 std range
+        diameters = np.clip(diameters, diameter_mean - diameter_std, diameter_mean + diameter_std)
+        intensities = (fixed_intensity * camera_distance**2) / (camera_distance + z_positions)**2
+
+        for _ in range(np.sum(n_list)-1):           
+            min_distance = 0
+
+#             change position until it is greater than distance parameter (x_y euclidean distance only)
+            while (min_distance < min_distance_param) | (max_distance_param < min_distance):
+                new_pos = np.random.random(2)*[image_w - 2*offset + offset, image_h - 2*offset + offset]
+                pos = x_y_positions.reshape(int(len(x_y_positions)/2), 2)
+                d = pos - new_pos
+                min_distance = np.sqrt(np.sum(d*d, axis=1)).min()
+                if (min_distance > min_distance_param) & (min_distance < max_distance_param):
+                    min_dist_list.append(min_distance)
+
+            x_y_positions = np.append(x_y_positions, new_pos)
+
+#         create object instances given the x,y,z position, label, diameter and intensity
+        x_y_positions = x_y_positions.reshape(np.sum(n_list), 2)
+        label_list = np.repeat(label_list, n_list).tolist()
+        objects.append([SpotObject(x,y,z, label, diameter, theta) for (x, y), z, label, diameter, theta in zip(x_y_positions, z_positions, label_list, diameters, intensities)])
+     
+    return np.array(objects), min_dist_list
+
+
 
 
 def create_rectangles(frames, image_w, image_h, offset, label_list, length_mean, length_std, density_range, n_list, distance = None):
@@ -306,7 +404,7 @@ def generateImage(objects, image_w, image_h, snr_range, impurity_objects = None)
 
 
 def exportConfig(file, nimages, image_w, image_h, image_d, 
-                 label_list, diameter_mean, diameter_std, density_range, distance,
+                 label_list, diameter_mean, diameter_std, density_range, min_distance, max_distance, particle_dist_4, particle_dist_3, particle_dist_2,
                  luminosity_range, snr_range, offset,
                  impurity_type, impurity_density = None, impurity_size_mean = None, impurity_size_std = None):
     config = configparser.ConfigParser()
@@ -322,7 +420,11 @@ def exportConfig(file, nimages, image_w, image_h, image_d,
     config.set('Section2', 'diameter_mean', str(diameter_mean))
     config.set('Section2', 'diameter_std', str(diameter_std))
     config.set('Section2', 'density_range', str(density_range))
-    config.set('Section2', 'distance', str(distance))
+    config.set('Section2', 'min_distance', str(min_distance))
+    config.set('Section2', 'max_distance', str(max_distance))
+    config.set('Section2', 'rate_particle_less_4_std', str(particle_dist_4))
+    config.set('Section2', 'rate_particle_less_3_std', str(particle_dist_3))
+    config.set('Section2', 'rate_particle_less_2_std', str(particle_dist_2))
     config.add_section('Section3')
     config.set('Section3', 'snr_range', str(snr_range))
     config.set('Section3', 'luminosity_range', str(luminosity_range))
@@ -375,6 +477,12 @@ def convert_xml_files_in_directory(xml_directory, txt_directory):
             convert_xml_to_txt(xml_file, txt_file)
 
 
+def less_than_rate(particle_distances, threshold):
+
+    number_interactions = len(particle_distances)
+    count = len([x for x in particle_distances if x < threshold])
+    return count / number_interactions   
+
 def create_images_wrapper(subdir,
                         nimages,
                         folders,
@@ -390,7 +498,10 @@ def create_images_wrapper(subdir,
                         luminosity_range,
                         density_range,
                         impurity_type,
-                        
+
+                        distance_factor = 1,
+                        max_distance_factor = None,
+
                         circle_impurities = False,
                         label_list_impurity_circle = None,
                         density_range_impurity_circle =  None,
@@ -402,6 +513,7 @@ def create_images_wrapper(subdir,
                         length_mean_impurity_rectangle = None,
                         length_std_impurity_rectangle = None):
     
+    particle_dist_list = []
     #create dirs
     if not os.path.exists(subdir):
         os.makedirs(subdir)
@@ -420,15 +532,23 @@ def create_images_wrapper(subdir,
         # for each image
         for i in tqdm(range(nimages[i])):
             # create particle objects
-            objects = create_particles(frames = 1, image_w= image_w, image_h= image_h, image_d= image_d, offset= offset,
-                        label_list= label_list, diameter_mean= diameter_mean, diameter_std= diameter_std, density_range = density_range,
-                        n_list = None, distance = None, luminosity_range = luminosity_range, fixed_camera_distance = 10)[0]
+            if max_distance_factor is None:
+                objects, particle_dist_list = create_particles(frames = 1, image_w= image_w, image_h= image_h, image_d= image_d, offset= offset,
+                            label_list= label_list, diameter_mean= diameter_mean, diameter_std= diameter_std, density_range = density_range,
+                            n_list = None, distance_factor = distance_factor, luminosity_range = luminosity_range, fixed_camera_distance = 10, min_dist_list = particle_dist_list)
             
+            else:
+                objects, particle_dist_list = create_particles_custom_overlap(frames = 1, image_w= image_w, image_h= image_h, image_d= image_d, offset= offset,
+                        label_list= label_list, diameter_mean= diameter_mean, diameter_std= diameter_std, density_range = density_range,
+                        n_list = None, distance_factor = distance_factor, max_distance_factor = max_distance_factor, luminosity_range = luminosity_range, fixed_camera_distance = 10, min_dist_list = particle_dist_list)
+            objects = objects[0]
+
             objects_impurities = None
             if circle_impurities:
-                objects_impurities = create_particles(frames = 1, image_w= image_w, image_h= image_h, image_d= image_d, offset= offset,
+                objects_impurities, _ = create_particles(frames = 1, image_w= image_w, image_h= image_h, image_d= image_d, offset= offset,
                                 label_list= label_list_impurity_circle, diameter_mean= diameter_mean_impurity_circle, diameter_std= diameter_std_impurity_circle, density_range = density_range_impurity_circle,
-                                n_list = None, distance = None, luminosity_range = [1,1], fixed_camera_distance = 10)[0]
+                                n_list = None, distance_factor = 1, luminosity_range = [1,1], fixed_camera_distance = 10)
+                objects_impurities = objects_impurities[0]
             if rectangle_impurities:
                 objects_impurities2 = create_rectangles(frames = 1, image_w= image_w, image_h= image_h, offset= offset,
                                 label_list= label_list_impurity_rectangle, length_mean= length_mean_impurity_rectangle, length_std= length_std_impurity_rectangle, density_range = density_range_impurity_rectangle,
@@ -446,6 +566,7 @@ def create_images_wrapper(subdir,
             
             if os.path.exists(fname):
                 os.remove(fname)
+                print(fname, 'deleted')
             plt.imsave(fname, image, cmap='gray')
 
             # create annotations xml
@@ -459,10 +580,11 @@ def create_images_wrapper(subdir,
         
         # create annotations txt
         convert_xml_files_in_directory(a_dir, t_dir)
-    
     # save run params
     exportConfig(file = subdir + 'info.txt', nimages = nimages, image_w =image_w, image_h =image_h, image_d =image_d, 
-                    label_list =label_list, diameter_mean =diameter_mean, diameter_std =diameter_std, density_range =density_range, distance = '1 particle',
+                    label_list =label_list, diameter_mean =diameter_mean, diameter_std =diameter_std, density_range =density_range, 
+                    min_distance = f'{distance_factor} particle(s)', max_distance = f'{max_distance_factor} particle(s)',
+                    particle_dist_4 = less_than_rate(particle_dist_list, diameter_mean*2), particle_dist_3 = less_than_rate(particle_dist_list, diameter_mean*1.5), particle_dist_2 = less_than_rate(particle_dist_list, diameter_mean),
                     luminosity_range =luminosity_range, snr_range =snr_range, offset =offset,
                     impurity_type =impurity_type, impurity_density = {'Circle': density_range_impurity_rectangle, "Rectangle": density_range_impurity_rectangle},
                     impurity_size_mean = {'Circle': diameter_mean_impurity_circle, "Rectangle": length_mean_impurity_rectangle},
@@ -524,3 +646,4 @@ if __name__ == "__main__":
     for key, value in test_data_params.items():
         print(key)
         create_images_wrapper(**value)
+
